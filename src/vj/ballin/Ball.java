bd73dee3b72f1fb97832a19b51ce064bb581a710
Dimi Lyb <dimitri.lybaert@student.ap.be>
2015-09-01 15:41:26 +0200

    java aan de praat

--- src/vj/ballin/Ball.java
+++ src/vj/ballin/Ball.java
@@ -1,6 +1,22 @@
 package vj.ballin;
 
-public class Ball {
-	
+import java.util.Random;
 
+import redis.clients.jedis.Jedis;
+
+public class Ball {
+	Random rand = new Random();
+	String antwoord = new String();
+	public String question(String quest) {
+		
+	//Connecting to Redis server on localhost
+    Jedis jedis = new Jedis("localhost");
+    antwoord = jedis.get(quest);
+    if(antwoord == null)
+    {
+    	jedis.set(quest, jedis.get("val" + rand.nextInt(20)));
+    }
+    //check whether server is running or not
+    return jedis.get(quest);
+	}
 }
---
+++ src/vj/ballin/RedisJava.java
@@ -0,0 +1,10 @@
+package vj.ballin;
+import redis.*;
+import redis.clients.jedis.Jedis;
+public class RedisJava {
+   public static void main(String[] args) {
+	   Ball b = new Ball();
+      System.out.println(b.question("Ga ik slagen2"));
+      
+ }
+}
